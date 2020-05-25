01844596d7fe983cf21dbfabd9244279315fb30d
alexandercortez <joelalexander10@outlook.com>
2020-05-25 16:56:11 -0600

    Implement Database Connection

---
+++ src/config/database/interfaces/database.interface.ts
@@ -0,0 +1,15 @@
+export type IDatabaseConfig = {
+  type: any | string,
+  host: string,
+  port?: any,
+  username: string,
+  password?: string,
+  database: string,
+  entities: Array<string>,
+  migrations?: Array<string>,
+  cli?: {
+    migrationsDir: string
+  }
+} & {
+  [key: string]: any
+}
